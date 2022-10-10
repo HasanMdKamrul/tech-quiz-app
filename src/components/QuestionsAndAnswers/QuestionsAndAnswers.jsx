9159a3db7c2d5a390006ac1405ebe34cd880844f
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-11 01:15:39 +0200

    Corect answer eye button implemented

--- src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,21 +1,43 @@
-import { EyeIcon } from '@heroicons/react/24/solid';
-import React from 'react';
-import OptionAnswer from '../OptionAnswer/OptionAnswer';
+import { EyeIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import { toast } from "react-toastify";
+import OptionAnswer from "../OptionAnswer/OptionAnswer";
 
+const QuestionsAndAnswers = ({
+  questionItem,
+  questionItem: { options, correctAnswer, question },
+}) => {
+  // console.log(questionItem)
 
-const QuestionsAndAnswers = ({questionItem,questionItem:{options,correctAnswer,question}}) => {
-    // console.log(questionItem)
-    return (
-        <div>
-            <div className='flex justify-between ' >
-            <h1 className='text-xl font-bold'>Question : {question}</h1>
-            <EyeIcon className='w-6 h-6 text-white'/>
-            </div>
-           {
-            options.map((option,index) => <OptionAnswer key={index} correctAnswer={correctAnswer} option={option}/>)
-           }
-        </div>
-    );
+  const correctAnswerDisplay = () => {
+    toast.info(`${correctAnswer}`, {
+      position: "top-right",
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      theme: "light",
+    });
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between ">
+        <h1 className="text-xl font-bold">Question : {question}</h1>
+        <EyeIcon
+          onClick={correctAnswerDisplay}
+          className="w-6 h-6 text-white"
+        />
+      </div>
+      {options.map((option, index) => (
+        <OptionAnswer
+          key={index}
+          correctAnswer={correctAnswer}
+          option={option}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default QuestionsAndAnswers;
