bd8f7f4c50dd36bd6e6c5e13fd26bf4bdb1f22c2
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-11 00:21:53 +0200

    Radio button added for the quiz answers

--- src/components/OptionAnswer/OptionAnswer.jsx
+++ src/components/OptionAnswer/OptionAnswer.jsx
@@ -4,11 +4,13 @@ const Options = ({option}) => {
 
     console.log(option)
 
+    const changeHandler = (event)=>{
+        console.log(event.target.value)
+    }
+
     return (
         <div>
-            <button className='bg-sky-500 m-2 p-2 rounded text-white'>
-                {option}
-            </button>
+            <input onChange={changeHandler} type="radio" value={option} /> {option}
         </div>
     );
 };
