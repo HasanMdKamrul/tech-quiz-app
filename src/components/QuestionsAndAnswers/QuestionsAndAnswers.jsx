3487e09c91b44862563f156e61d350efb9461218
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-11 02:10:45 +0200

    EyeIcon design fixes

--- src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -25,11 +25,16 @@ const QuestionsAndAnswers = () => {
   return (
     <div className="bg-sky-100 p-5 rounded-2xl m-2">
       <div className="flex justify-between ">
+        <div>
         <h1 className="text-xl font-bold">Question : {question}</h1>
+        </div>
+        <div>
         <EyeIcon
           onClick={correctAnswerDisplay}
           className="w-6 h-6 text-black"
         />
+        </div>
+        
       </div>
       <div >
       {options.map((option, index) => (
--- src/components/QuizPlayGround/QuizPlayGround.jsx
+++ src/components/QuizPlayGround/QuizPlayGround.jsx
@@ -4,38 +4,37 @@ import { IndividualTopicContext } from "../../context/Context";
 import QuestionsAndAnswers from "../QuestionsAndAnswers/QuestionsAndAnswers";
 
 const QuizPlayGround = () => {
-    const {data} = useLoaderData();
+  const { data } = useLoaderData();
 
-   
-  const  {name,questions}   = data;
+  const { name, questions } = data;
 
-//   console.log(questions)
+  //   console.log(questions)
 
-  
   return (
-    
-         <div>
-      <h1 className="text-5xl font-extrabold text-center my-5">Quiz and Answer</h1>
+    <div>
+      <h1 className="text-5xl font-extrabold text-center my-5">
+        Quiz and Answer
+      </h1>
       <div>
-        <h2 className="text-center text-3xl font-blod my-5">Quiz Related to - {name}</h2>
-       <div className="flex justify-center">
-        <div className="grid md:grid-cols-1 p-5 text-black ">
-                {
-                    questions.map(questionItem => {
-                        console.log(questionItem.id)
-                        return(
-                            <IndividualTopicContext.Provider key={questionItem.id} value={questionItem}>
-                                <QuestionsAndAnswers   />
-                            </IndividualTopicContext.Provider>
-                        )
-                    })
-                }
-            </div>
-       </div>
+        <h2 className="text-center text-3xl font-blod my-5">
+          Quiz Related to - {name}
+        </h2>
+        <div className="flex justify-center">
+          <div className="grid md:grid-cols-1 p-5 text-black ">
+            {questions.map((questionItem) => {
+              return (
+                <IndividualTopicContext.Provider
+                  key={questionItem.id}
+                  value={questionItem}
+                >
+                  <QuestionsAndAnswers />
+                </IndividualTopicContext.Provider>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
-    
-   
   );
 };
 
