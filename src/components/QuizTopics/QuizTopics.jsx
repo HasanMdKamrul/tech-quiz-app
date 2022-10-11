4d54284a7e816e8f3f1ef754a0ebcdfa3369fc6f
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-11 06:19:48 +0200

    few design fixes done and probable final push

--- src/components/QuizTopics/QuizTopics.jsx
+++ src/components/QuizTopics/QuizTopics.jsx
@@ -9,7 +9,7 @@ const QuizTopics = () => {
     <>
       <h1 className="text-5xl font-extrabold text-center">Tech Quizes</h1>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-8 lg:grid-cols-4 sm:max-w-sm sm:mx-auto lg:max-w-full">
+        <div className="grid gap-8  lg:grid-cols-4 sm:max-w-sm sm:mx-auto  md:max-w-full">
           {quizes.map((quiz) => (
             <QuizCard key={quiz.id} quiz={quiz} />
           ))}
