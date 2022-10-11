8540052ada5123df2190adf8b35b55d437485c83
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-11 02:27:13 +0200

    Functional error page added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import Lottie from "lottie-react";
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import errorAnimation from "../../assests/errorAnimation.json";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>Error Page</h1>
-        </div>
-    );
+  const { status, statusText, message } = useRouteError();
+  const error = useRouteError();
+
+  return (
+    <div>
+      {error && (
+        <section className="flex items-center h-full p-16">
+          <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+            <div className="max-w-md text-center">
+              <h2 className="mb-8 font-extrabold text-9xl">
+                <span className="sr-only">Error</span>
+                <Lottie animationData={errorAnimation} />
+              </h2>
+              <p className="text-2xl font-semibold md:text-3xl">
+                Sorry, we couldn't find this page.{" "}
+                <span className="text-red-500"> {statusText || message}</span>{" "}
+              </p>
+              <p className="mt-4 mb-8 dark:text-gray-400">{status}</p>
+              <Link
+                rel="noopener noreferrer"
+                to="/"
+                className="px-8 py-3 font-semibold rounded bg-sky-400 text-black"
+              >
+                Back to homepage
+              </Link>
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
+  );
 };
 
 export default ErrorPage;
